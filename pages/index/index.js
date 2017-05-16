7e9edbeb66823b5403531da1268ced2812732928
邵磊 <shao8@vip.qq.com>
2017-05-16 10:34:26 +0800

    first

---
+++ pages/index/index.js
@@ -0,0 +1,65 @@
+var wc = require('../../utils/wcache.js')
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {}
+  },
+  //事件处理函数
+  bindViewTap: function () {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+  }, get(e) {
+    this.setData({
+      text: null
+    });
+    switch (e.currentTarget.dataset.type) {
+      case "def":
+        this.setData({
+          text: wc.get('k')
+        });
+        break;
+      case "string":
+        this.setData({
+          text: wc.get('k', '你好')
+        });
+        break;
+      case "json":
+        this.setData({
+          text: wc.get('k', { "a": "1" })
+        });
+        break;
+    }
+
+  }, put(e) {
+    console.log(e);
+    switch (e.currentTarget.dataset.type) {
+      case "string":
+        wc.put('k', 'string你好啊');
+        break;
+      case "json":
+        wc.put('k', { "b": "3" }, 2);
+        break;
+      case "list":
+        wc.put('k', [1, 2, 3]);
+        break;
+      case "boolean":
+        wc.put('k', true);
+        break;
+    }
+
+
+
+
+
+    wx.showToast({
+      title: '存储成功',
+      duration: 500,
+    })
+  }
+
+})
---
+++ utils/wcache.js
@@ -0,0 +1,50 @@
+var postfix = '_deadtime';
+
+function put(k, v, t) {
+  // console.log(k);
+  wx.setStorageSync(k, v)
+  var seconds = parseInt(t);
+  if (seconds > 0) {
+    var timestamp = Date.parse(new Date());
+    timestamp = timestamp / 1000 + seconds;
+    // console.log(timestamp);
+    wx.setStorageSync(k + postfix, timestamp + "")
+  } else {
+    wx.removeStorageSync(k + postfix)
+  }
+}
+
+function get(k) {
+  get(k, '');
+}
+
+function get(k, def) {
+  var deadtime = parseInt(wx.getStorageSync(k + postfix))
+  if (deadtime) {
+    if (parseInt(deadtime) < Date.parse(new Date()) / 1000) {
+      if (def) { return def; } else { return; }
+    }
+  }
+  var res = wx.getStorageSync(k);
+  if (res) {
+    return res;
+  } else {
+    return def;
+  }
+}
+
+function remove(k) {
+  wx.removeStorageSync(k);
+  wx.removeStorageSync(k + postfix);
+}
+
+function clear() {
+  wx.clearStorageSync();
+}
+
+module.exports = {
+  put: put,
+  get: get,
+  remove: remove,
+  clear: clear,
+}
